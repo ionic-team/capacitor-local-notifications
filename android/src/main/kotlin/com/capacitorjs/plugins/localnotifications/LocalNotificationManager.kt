b4afb9109a15526a548465df09b7da5bfb1120fa
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2026-07-23 17:00:22 -0400

    fix(android): properly cancel alarm

--- android/src/main/kotlin/com/capacitorjs/plugins/localnotifications/LocalNotificationManager.kt
+++ android/src/main/kotlin/com/capacitorjs/plugins/localnotifications/LocalNotificationManager.kt
@@ -419,6 +419,7 @@ class LocalNotificationManager(
         if (pi != null) {
             val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
             alarmManager.cancel(pi)
+            pi.cancel()
         }
     }
 
