0d86d533883bc6c11e451ae90cb88423fd8aeeea
Mark Anderson <mark.anderson@outsystems.com>
2026-09-16 14:05:10 -0400

    chore: remove SDK_O version checks

--- android/src/main/kotlin/com/capacitorjs/plugins/localnotifications/NotificationChannelManager.kt
+++ android/src/main/kotlin/com/capacitorjs/plugins/localnotifications/NotificationChannelManager.kt
@@ -30,7 +30,6 @@ class NotificationChannelManager {
     }
 
     fun createChannel(call: PluginCall) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = JSObject()
             if (call.getString(CHANNEL_ID) != null) {
                 channel.put(CHANNEL_ID, call.getString(CHANNEL_ID))
@@ -54,81 +53,68 @@ class NotificationChannelManager {
             channel.put(CHANNEL_LIGHT_COLOR, call.getString(CHANNEL_LIGHT_COLOR, null))
             createChannel(channel)
             call.resolve()
-        } else {
-            call.unavailable()
-        }
     }
 
     fun createChannel(channel: JSObject) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                channel.getString(CHANNEL_ID),
-                channel.getString(CHANNEL_NAME),
-                channel.getInteger(CHANNEL_IMPORTANCE) ?: NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationChannel.description = channel.getString(CHANNEL_DESCRIPTION)
-            notificationChannel.lockscreenVisibility = channel.getInteger(CHANNEL_VISIBILITY) ?: NotificationCompat.VISIBILITY_PUBLIC
-            notificationChannel.enableVibration(channel.getBool(CHANNEL_VIBRATE) ?: false)
-            notificationChannel.enableLights(channel.getBool(CHANNEL_USE_LIGHTS) ?: false)
-            val lightColor = channel.getString(CHANNEL_LIGHT_COLOR)
-            if (lightColor != null) {
-                try {
-                    notificationChannel.lightColor = Color.parseColor(lightColor)
-                } catch (ex: IllegalArgumentException) {
-                    Logger.error(Logger.tags("NotificationChannel"), "Invalid color provided for light color.", null)
-                }
+        val notificationChannel = NotificationChannel(
+            channel.getString(CHANNEL_ID),
+            channel.getString(CHANNEL_NAME),
+            channel.getInteger(CHANNEL_IMPORTANCE) ?: NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationChannel.description = channel.getString(CHANNEL_DESCRIPTION)
+        notificationChannel.lockscreenVisibility = channel.getInteger(CHANNEL_VISIBILITY) ?: NotificationCompat.VISIBILITY_PUBLIC
+        notificationChannel.enableVibration(channel.getBool(CHANNEL_VIBRATE) ?: false)
+        notificationChannel.enableLights(channel.getBool(CHANNEL_USE_LIGHTS) ?: false)
+        val lightColor = channel.getString(CHANNEL_LIGHT_COLOR)
+        if (lightColor != null) {
+            try {
+                notificationChannel.lightColor = Color.parseColor(lightColor)
+            } catch (ex: IllegalArgumentException) {
+                Logger.error(Logger.tags("NotificationChannel"), "Invalid color provided for light color.", null)
             }
-            var sound = channel.getString(CHANNEL_SOUND, null)
-            if (sound != null && sound.isNotEmpty()) {
-                if (sound.contains(".")) {
-                    sound = sound.substring(0, sound.lastIndexOf('.'))
-                }
-                val audioAttributes = AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-                    .build()
-                val soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.packageName + "/raw/" + sound)
-                notificationChannel.setSound(soundUri, audioAttributes)
-            }
-            notificationManager.createNotificationChannel(notificationChannel)
         }
+        var sound = channel.getString(CHANNEL_SOUND, null)
+        if (sound != null && sound.isNotEmpty()) {
+            if (sound.contains(".")) {
+                sound = sound.substring(0, sound.lastIndexOf('.'))
+            }
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build()
+            val soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.packageName + "/raw/" + sound)
+            notificationChannel.setSound(soundUri, audioAttributes)
+        }
+        notificationManager.createNotificationChannel(notificationChannel)
     }
 
     fun deleteChannel(call: PluginCall) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = call.getString("id")
-            notificationManager.deleteNotificationChannel(channelId)
-            call.resolve()
-        } else {
-            call.unavailable()
-        }
+        val channelId = call.getString("id")
+        notificationManager.deleteNotificationChannel(channelId)
+        call.resolve()
     }
 
     fun listChannels(call: PluginCall) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannels = notificationManager.notificationChannels
-            val channels = JSArray()
-            for (notificationChannel in notificationChannels) {
-                val channel = JSObject()
-                channel.put(CHANNEL_ID, notificationChannel.id)
-                channel.put(CHANNEL_NAME, notificationChannel.name)
-                channel.put(CHANNEL_DESCRIPTION, notificationChannel.description)
-                channel.put(CHANNEL_IMPORTANCE, notificationChannel.importance)
-                channel.put(CHANNEL_VISIBILITY, notificationChannel.lockscreenVisibility)
-                channel.put(CHANNEL_SOUND, notificationChannel.sound)
-                channel.put(CHANNEL_VIBRATE, notificationChannel.shouldVibrate())
-                channel.put(CHANNEL_USE_LIGHTS, notificationChannel.shouldShowLights())
-                channel.put(CHANNEL_LIGHT_COLOR, String.format("#%06X", 0xFFFFFF and notificationChannel.lightColor))
-                Logger.debug(Logger.tags("NotificationChannel"), "visibility " + notificationChannel.lockscreenVisibility)
-                Logger.debug(Logger.tags("NotificationChannel"), "importance " + notificationChannel.importance)
-                channels.put(channel)
-            }
-            val result = JSObject()
-            result.put("channels", channels)
-            call.resolve(result)
-        } else {
-            call.unavailable()
+        val notificationChannels = notificationManager.notificationChannels
+        val channels = JSArray()
+        for (notificationChannel in notificationChannels) {
+            val channel = JSObject()
+            channel.put(CHANNEL_ID, notificationChannel.id)
+            channel.put(CHANNEL_NAME, notificationChannel.name)
+            channel.put(CHANNEL_DESCRIPTION, notificationChannel.description)
+            channel.put(CHANNEL_IMPORTANCE, notificationChannel.importance)
+            channel.put(CHANNEL_VISIBILITY, notificationChannel.lockscreenVisibility)
+            channel.put(CHANNEL_SOUND, notificationChannel.sound)
+            channel.put(CHANNEL_VIBRATE, notificationChannel.shouldVibrate())
+            channel.put(CHANNEL_USE_LIGHTS, notificationChannel.shouldShowLights())
+            channel.put(CHANNEL_LIGHT_COLOR, String.format("#%06X", 0xFFFFFF and notificationChannel.lightColor))
+            Logger.debug(Logger.tags("NotificationChannel"), "visibility " + notificationChannel.lockscreenVisibility)
+            Logger.debug(Logger.tags("NotificationChannel"), "importance " + notificationChannel.importance)
+            channels.put(channel)
         }
+        val result = JSObject()
+        result.put("channels", channels)
+        call.resolve(result)
     }
 
     companion object {
