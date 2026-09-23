4236b5868e7584d7683333d6761cbf1a39dc0c80
Mark Anderson <mark.anderson@outsystems.com>
2026-09-23 12:46:35 -0400

    update example app
    Signed-off-by: Mark Anderson <mark.anderson@outsystems.com>

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -4,22 +4,21 @@ import PackageDescription
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
     name: "CapApp-SPM",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapApp-SPM",
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "9.0.0-nightly-20260211T152431.0"),
+        .package(url: "https://github.com/ionic-team/capacitor", exact: "9.0.0-alpha.7"),
         .package(name: "CapacitorLocalNotifications", path: "../../../..")
     ],
     targets: [
         .target(
             name: "CapApp-SPM",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "Capacitor", package: "capacitor"),
                 .product(name: "CapacitorLocalNotifications", package: "CapacitorLocalNotifications")
             ]
         )
